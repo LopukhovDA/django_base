76a7db4b708f54abf18946aa326f0fd47a4a0af1
Dmitriy <lopukhovda@gmail.com>
2022-02-10 16:01:40 +0300

    lesson7_homework
    Выполнена домашняя работа к 7му уроку

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -23,12 +23,12 @@ from django.conf.urls import include
 
 urlpatterns = [
     path("", mainapp.main, name="main"),
-    # path("products/", mainapp.products, name="products"),
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
-    path("admin/", admin.site.urls),
+    #  path("admin/", admin.site.urls),
     path("auth/", include("authapp.urls", namespace="auth")),
     path("basket/", include("basketapp.urls", namespace="basket")),
+    path("admin/", include("adminapp.urls", namespace="admin")),
 ]
 
 if settings.DEBUG:
