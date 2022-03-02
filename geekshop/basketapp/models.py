78d84bf512d68be4413572e039c0ab931aa715fb
Dmitriy <lopukhovda@gmail.com>
2022-03-02 23:37:31 +0300

    lesson4_homework
    Домашняя работа к 4-му уроку выполнена

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -3,13 +3,35 @@ from django.conf import settings
 from mainapp.models import Product
 
 
+class BasketQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
+    objects = BasketQuerySet.as_manager()
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket"
     )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
-    add_datetime = models.DateTimeField(verbose_name="время", auto_now_add=True)
+    quantity = models.PositiveIntegerField(
+        verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(
+        verbose_name="время", auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            basket_item = Basket.objects.filter(pk=self.pk)
+            self.product.quantity -= self.quantity - \
+                basket_item[0].quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
 
     @property
     def product_cost(self):
